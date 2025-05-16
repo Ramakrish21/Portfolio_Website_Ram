6d722dc066e9efdbd76021aa2d20744a842121c9
Shiva Ramakrishna Reddy <169166354+Ramakrish21@users.noreply.github.com>
2025-05-16 15:47:10 +0530

    Home Page Updated

--- src/component/LoadingScreen.jsx
+++ src/component/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fullText = "<Initializing RamaKrishna's Portfolio />";
+  const fullText = "<Initializing Portfolio />";
 
   useEffect(() => {
     let index = 0;
@@ -15,7 +15,7 @@ export const LoadingScreen = ({ onComplete }) => {
 
         setTimeout(() => {
           onComplete();
-        }, 1000);
+        }, 1000);       
       }
     }, 100);
 
